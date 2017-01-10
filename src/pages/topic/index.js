fb6908e136acb07163781dc62e171846fa295d56
waha3 <895240740@qq.com>
2017-01-10 18:00:24 +0800

    hanxin

--- src/pages/topic/index.js
+++ src/pages/topic/index.js
@@ -1,7 +1,21 @@
+const api = require('../../api/index.js');
 const app = getApp();
 
 Page({
   data: {
-    title: 'hello world'
+    title: 'hello world',
+    id: '',
+    content: {}
+  },
+  onLoad(e) {
+    this.setData({
+      id: e.id
+    });
+
+    const { id } = this.data;
+    const path = `topic/${id}`;
+    api.get(path).then(res => this.setData({
+      content: res.data.data
+    }));
   }
 });
